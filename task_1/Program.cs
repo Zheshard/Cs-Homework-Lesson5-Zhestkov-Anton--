88e4d69b4aa7c5294a5e5f853f589caa1ecff07e
Zheshard <antonzestkov489@gmail.com>
2022-12-15 21:27:56 +0300

    Added method fill array with random number

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая показывает количество четных чисел в массиве, заполненном случайными положительными трехзначными числами.
+
+const int SIZE_ARRAY = 10;
+const int LEFT_RANGE_ARRAY = 100;
+const int RIGTH_RANGE_ARRAY = 999;
+int[] arrayPositive3DigitNumbers = FillArrayWithRandomNumbers(SIZE_ARRAY, LEFT_RANGE_ARRAY, RIGTH_RANGE_ARRAY);
+PrintArray(arrayPositive3DigitNumbers);
+
+int[] FillArrayWithRandomNumbers(int size, int leftRange, int rigthRange)
+{
+	int[] fillableArray = new int[size];
+	Random rand = new Random();
+	for (int i = 0; i < fillableArray.Length; i++)
+	{
+		fillableArray[i] = rand.Next(leftRange, rigthRange + 1);
+	}
+	return fillableArray;
+}
+
+void PrintArray(int[] outputArray)
+{
+	System.Console.WriteLine(string.Join("; ", outputArray));
+}
