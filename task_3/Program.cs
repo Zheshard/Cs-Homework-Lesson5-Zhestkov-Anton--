241828c88d106d3c99ca12ef82ffc2c1dc5a89c2
Zheshard <antonzestkov489@gmail.com>
2022-12-15 22:50:42 +0300

    Added methods for generating an array of real numbers and outputting an array to the console

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -1,2 +1,39 @@
-﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
-Console.WriteLine("Hello, World!");
+﻿// Программа, вычисляющая разницу между максимальным и минимальным элементами массива, состоящего из вещественных чисел.
+
+int sizeArray = InputNumber("Введите размер массива: ");
+int leftRangeArray = InputNumber("Введите значение начала диапазона: ");
+int rigthRangeArray = InputNumber("Введите значение конца диапазона: ");
+double[] arrayRealNumbers = FillArrayWithRandomRealNumbers(sizeArray, leftRangeArray, rigthRangeArray);
+PrintArray(arrayRealNumbers);
+
+double[] FillArrayWithRandomRealNumbers(int size, int leftRange, int rigthRange)
+{
+	double[] fillableArray = new double[size];
+	Random rand = new Random();
+	for (int i = 0; i < fillableArray.Length; i++)
+	{
+		fillableArray[i] = Math.Round(rand.Next(leftRange, rigthRange) + rand.NextDouble(), 2);
+	}
+	return fillableArray;
+}
+
+void PrintArray(double[] outputArray)
+{
+	System.Console.WriteLine(string.Join("; ", outputArray));
+}
+
+int InputNumber(string invitationText)
+{
+	int inputNum;
+	while (true)
+	{
+		System.Console.WriteLine(invitationText);
+		string inputStr = Console.ReadLine();
+		if (int.TryParse(inputStr, out inputNum))
+		{
+			break;
+		}
+		System.Console.WriteLine("Введено неверное значение. Необходимо ввести число!");
+	}
+	return inputNum;
+}
